c161f91bb9d98d724bd2672ca4b72a76f2a43e56
John Mc Kie <john.mckie@resourcegenius.com>
2011-03-07 19:51:50 -0700

    Initial Commit 03072011

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rge3Sample::Application.config.secret_token = 'ccac0bea61fd3756eb680954b1af8ac0d87cbf0b7deca239d73bde63473867b9a9ef11f7189a84a2b569b23678edb8df6ecf8d35858bf27ba7e28902289e58e3'
